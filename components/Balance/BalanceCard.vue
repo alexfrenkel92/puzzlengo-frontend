b0719c31c699095423b70e67cdb8c8b57c58f978
zsofi.kovats <zsofi.kovats@dataexpert.hu>
2021-01-14 15:08:51 +0100

    balance cards added

---
+++ components/Balance/BalanceCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="344"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">
+            {{ title }}
+          </div>
+          <v-list-item-title class="headline mb-1">
+            {{ total }} points
+          </v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-avatar
+          tile
+          size="80"
+          color="#7ABE8F"
+        >
+          <v-icon x-large>
+            {{ icon }}
+          </v-icon>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn @click="show = !show"> Details </v-btn>
+        <v-spacer />
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider />
+          <v-card-text>
+            <v-data-table
+              :headers="headers"
+              :items="details"
+              hide-default-footer
+              class="elevation-1"
+            />
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'total', 'details', 'icon'],
+  data() {
+    return {
+      show: false,
+      headers: [{ text: 'Value', value: 'value' }, { text: 'Date', value: 'date' }]
+    }
+  },
+  methods: {
+    getMore() {
+      this.$router.push('/dashboard')
+    },
+    spend() {
+      console.log('spending money')
+    }
+  }
+}
+</script>
---
+++ pages/profile/balance.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="balanceContainer">
+    <Balance :icon="'mdi-currency-usd-circle-outline'" :title="'My balance'" :total="total" :details="details" />
+    <Balance :icon="'mdi-chevron-up-circle-outline'" :title="'Total credits earned'" :total="totalEarned" :details="earn" />
+    <Balance :icon="'mdi-chevron-down-circle-outline'" :title="'Total credits spent'" :total="totalSpent" :details="spend" />
+
+  </div>
+</template>
+
+<script>
+import Balance from '@/components/Balance/BalanceCard'
+export default {
+  components: {
+    Balance
+  },
+  data() {
+    return {
+      details: [{ value: 100, date: '01.03.' }, { value: -150, date: '01.03.' }, { value: 60, date: '01.03.' }, { value: -20, date: '01.03.' }],
+      total: 0,
+      totalEarned: 0,
+      totalSpent: 0
+    }
+  },
+  computed: {
+    earn() {
+      return this.details.filter((d) => {
+        return d.value > 0
+      })
+    },
+    spend() {
+      return this.details.filter((d) => {
+        return d.value < 0
+      })
+    }
+  },
+  created() {
+    this.details.forEach((element) => {
+      this.total += element.value
+      if (element.value > 0) {
+        this.totalEarned += element.value
+      } else {
+        this.totalSpent += element.value
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .balanceContainer{
+    margin-top: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around
+  }
+</style>
