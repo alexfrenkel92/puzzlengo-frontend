2b468462a52fe0d130c26e37d9e3ff16fe115fda
DE061\FrenkelA <alex.frenkel@dataexpert.hu>
2021-01-26 09:35:41 +0100

    footer restructured

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,30 +1,76 @@
 <template>
-  <v-footer padless color="#E0F2F1">
-    <v-col
-      class="text-center"
-      cols="12"
-    >
+  <v-footer padless>
+    <div class="image">
+      <img :src="logo" :alt="logo" width="80px" height="80px">
+      <span>Some motto xyz</span>
       <span>&copy; {{ new Date().getFullYear() }}</span>
-      <nuxt-link to="/privacy">Privacy Policy</nuxt-link>
-      <nuxt-link to="/terms">Terms of Service</nuxt-link>
-    </v-col>
+    </div>
+    <div class="column1">
+      <ul>
+        <li>
+          <nuxt-link to="">About Us</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="">Terms of Service</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="">Privacy Policy</nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <div class="column2">
+      <ul>
+        <li>
+          <nuxt-link to="">FAQ</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="">GDPR</nuxt-link>
+        </li>
+      </ul>
+    </div>
   </v-footer>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    logo() {
+      return require('~/static/images/de_emblema_RGB.png')
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.v-footer {
+  height: fit-content;
+  background-color: #cfd1d1 !important;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  padding: 30px 5px 20px 5px;
+}
+ul {
+  list-style-type: none;
+}
+li {
+  padding-bottom: 10px;
+}
 a {
   text-decoration: none;
   color: black !important;
-  margin: 0 10px;
 }
-.v-footer {
-  height: 100px;
-  background-color: #E0F2F1;
+.image,
+.column1,
+.column2 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5px 10px 5px;
+}
+img {
+  margin-bottom: 10px;
 }
 </style>
