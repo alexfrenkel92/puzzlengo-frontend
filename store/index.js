1875bc7315aea42f9ef4839b2050f9a5bb512b2b
DE061\FrenkelA <alex.frenkel@dataexpert.hu>
2021-02-26 16:57:59 +0100

    some styling

--- store/index.js
+++ store/index.js
@@ -1,17 +1,20 @@
 import Vuex from 'vuex'
 
-import authModule from './auth.js'
+import Cookies from 'js-cookie'
+
+// import authModule from './auth.js'
 import surveysModule from './surveys.js'
 import userProfileModule from './userProfile.js'
 import tangoCardModule from './tangoCards.js'
 import activeTabNrModule from './activeTabNr'
 import brakePointsModule from './breakPoints'
 import settingsModule from './settings.js'
+import { cookieFromRequest } from '~/utils'
 
 const createStore = () => {
   return new Vuex.Store({
     modules: {
-      auth: authModule,
+      // auth: authModule,
       surveys: surveysModule,
       userProfile: userProfileModule,
       tangoCards: tangoCardModule,
@@ -29,6 +32,31 @@ const createStore = () => {
         smDown: ['smDown', 'xsDown'],
         mdDown: ['mdDown', 'smDown', 'xsDown'],
         lgDown: ['lgDown', 'mdDown', 'smDown', 'xsDown']
+      }
+    },
+    actions: {
+      nuxtServerInit({ commit }, { req }) {
+        const token = cookieFromRequest(req, 'token')
+        if (token) {
+          commit('auth/SET_TOKEN', token)
+        }
+
+        // const locale = cookieFromRequest(req, 'locale')
+        // if (locale) {
+        //   commit('lang/SET_LOCALE', { locale })
+        // }
+      },
+
+      nuxtClientInit({ commit, getters }) {
+        const token = Cookies.get('token')
+        if (token && !getters['auth/token']) {
+          commit('auth/SET_TOKEN', token)
+        }
+
+        // const locale = Cookies.get('locale')
+        // if (locale && !getters['lang/locale']) {
+        //   commit('lang/SET_LOCALE', { locale })
+        // }
       }
     }
   })
